546f96e3ced147afded2da93cc5f3e2372ed1137
Jan Jurča <jjurcaj@gmail.com>
2024-04-19 09:21:30 +0200

    Refactor truncate() function in FileTree class and fix calculation of end pointer

--- include/filestorm/filetree.h
+++ include/filestorm/filetree.h
@@ -93,16 +93,13 @@ public:
     void truncate(std::uintmax_t new_size) {
       // There is the need to adjust the fallocation number according to new truncated size
       std::uintmax_t old_size = size();
+      if (new_size > old_size) {
+        return;
+      }
       int index = 1;
       std::uintmax_t hole_size = old_size / 2;
       std::uintmax_t start = 0, end = hole_size;
       while (end < new_size) {
-        if (index < 50) {
-          spdlog::debug("start: {}, end: {}, hole_size: {}, new_size: {}, index: {}, old_size: {}", start, end, hole_size, new_size, index, old_size);
-        } else {
-          throw std::runtime_error("Too many iterations");
-        }
-
         start = end;
         hole_size = hole_size / 2;
         end = start + hole_size;
