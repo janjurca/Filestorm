be411284a7323713a13b2499f914dcd7c1698993
Jan Jurča <jjurcaj@gmail.com>
2025-04-22 10:16:58 +0200

    result save on ctrl-c is back. a little hacky but working

--- standalone/source/interupts.h
+++ standalone/source/interupts.h
@@ -1,13 +1,21 @@
 #pragma once
 
+#include <filestorm/result.h>
 #include <filestorm/utils/logger.h>
+#include <fmt/format.h>
+
+#include <iostream>
+#include <string>
 
 #include "config.h"
 
 void sigint_handler(int signum) {
   logger.set_progress_bar(nullptr);
-  logger.info("SIGINT received. Saving up to now results...");
-  // config.selected_senario->save();
-  // config.selected_senario->print();
+  logger.info("SIGINT received. Here are the results up to now...");
+  Result::print();
+  std::string filename = fmt::format("results_unfinished_{}.json", std::time(nullptr));
+  logger.info("Saving results to {}", filename);
+  Result::save(filename);
+
   exit(signum);
 }
