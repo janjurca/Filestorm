eb3421ba73b4306f6287ef1401403dd8adfc705a
Jan Jurča <jjurcaj@gmail.com>
2025-09-12 22:21:08 +0200

    Update rapid aging max time parameter to 60 minutes

--- source/scenarios/aging.cpp
+++ source/scenarios/aging.cpp
@@ -51,7 +51,8 @@ AgingScenario::AgingScenario() {
   addParameter(Parameter("", "cleanup", "Should clean up files/folders after the test is done", "true"));
   addParameter(Parameter("", "rapid-aging-threshold", "Set threshold for rapid aging testing 90-0.where 90 is rapid aging essentially turned off and at 0  will probably never ends.", "37"));
   addParameter(Parameter("", "rapid-aging-min-time", "Minimal time to run rapid aging in seconds", "5s"));
-  addParameter(Parameter("", "rapid-aging-max-time", "Maximal time to run rapid aging in seconds", "10m"));
+  addParameter(Parameter("", "rapid-aging-max-time", "Maximal time to run rapid aging in seconds", "60m"));
+
   addParameter(Parameter("", "settings-safe-margin",
                          "When new file is computed and there is not enough space the new file size is shrinked to available size but in some cases the fs has a file size overhead because of "
                          "metadata writes which are hard to predict and compute. So the safe margin is introduced which specify what is a minimal space amount that should be left available",
